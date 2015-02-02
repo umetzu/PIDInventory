697f5fb87d24e49367f50b373b0e0ccd7911b278
umetzu <dennis@umetzu.com>
2015-02-02 17:04:53 -0500

    Broken

--- PIDInventory/PIDCase.swift
+++ PIDInventory/PIDCase.swift
@@ -2,7 +2,7 @@
 //  PIDInventory.swift
 //  PIDInventory
 //
-//  Created by Baker on 1/30/15.
+//  Created by Baker on 2/2/15.
 //  Copyright (c) 2015 Umetzu. All rights reserved.
 //
 
@@ -11,42 +11,51 @@ import CoreData
 
 class PIDCase: NSManagedObject {
 
-    @NSManaged var insertBarcode: String
-    @NSManaged var caseBent: Bool
+    @NSManaged var inventoryCaseBarcode: String
     @NSManaged var caseBroken: Bool
-    @NSManaged var caseColor: Int32
-    @NSManaged var caseComingApart: Bool
-    @NSManaged var caseCondition: Int32
+    @NSManaged var caseColor: String
+    @NSManaged var caseSeverity: String
     @NSManaged var caseGraffiti: Bool
     @NSManaged var caseOther: Bool
-    @NSManaged var casePitted: Bool
     @NSManaged var caseRusted: Bool
-    @NSManaged var caseUnauthorized: Bool
-    @NSManaged var comments: String
-    @NSManaged var coverCondition: Int32
+    @NSManaged var inventoryComments: String
+    @NSManaged var coverSeverity: String
     @NSManaged var coverCracked: Bool
     @NSManaged var coverDiscolored: Bool
     @NSManaged var coverGraffiti: Bool
     @NSManaged var coverNoCover: Bool
     @NSManaged var coverOther: Bool
-    @NSManaged var coverUnauthorized: Bool
     @NSManaged var id: Int32
-    @NSManaged var insertCondition: Int32
-    @NSManaged var insertDescription: String
+    @NSManaged var inventoryInsertBarcode: String
+    @NSManaged var insertComments: String
     @NSManaged var insertFaded: Bool
     @NSManaged var insertMissing: Bool
+    @NSManaged var insertName: Int32
     @NSManaged var insertOther: Bool
     @NSManaged var insertTorn: Bool
-    @NSManaged var latitude: Double
-    @NSManaged var longitude: Double
-    @NSManaged var caseBarcode: String
+    @NSManaged var inventoryLatitude: Double
+    @NSManaged var inventoryLongitude: Double
     @NSManaged var standBroken: Bool
-    @NSManaged var standCondition: Int32
+    @NSManaged var standSeverity: String
     @NSManaged var standGraffiti: Bool
     @NSManaged var standOther: Bool
     @NSManaged var standRusted: Bool
     @NSManaged var standRustedBasePlate: Bool
-    @NSManaged var standUnauthorized: Bool
-    @NSManaged var insertName: String
+    @NSManaged var inventoryDate: NSTimeInterval
+    @NSManaged var inventoryCaseNameArchive: String
+    @NSManaged var locationDescription: String
+    @NSManaged var locationOrientation: String
+    @NSManaged var locationAdjacentTVM: Bool
+    @NSManaged var locationMountType: String
+    @NSManaged var caseWidth: Double
+    @NSManaged var caseSide: Int32
+    @NSManaged var insertCategory: String
+    @NSManaged var locationCasesInCluster: Int32
+    @NSManaged var locationPositionInCluster: Int32
+    @NSManaged var inventoryUser: String
+    @NSManaged var inventoryPhoto1: String
+    @NSManaged var inventoryPhoto2: String
+    @NSManaged var insertDate: NSTimeInterval
+    @NSManaged var insertWaterDamage: Bool
 
 }
--- PIDInventory/PIDInsert.swift
+++ PIDInventory/PIDInsert.swift
@@ -2,7 +2,7 @@
 //  PIDInventory.swift
 //  PIDInventory
 //
-//  Created by Baker on 1/30/15.
+//  Created by Baker on 2/2/15.
 //  Copyright (c) 2015 Umetzu. All rights reserved.
 //
 
@@ -11,8 +11,10 @@ import CoreData
 
 class PIDInsert: NSManagedObject {
 
+    @NSManaged var barcode: String
     @NSManaged var id: Int32
     @NSManaged var name: String
-    @NSManaged var barcode: String
+    @NSManaged var date: NSTimeInterval
+    @NSManaged var category: String
 
 }
